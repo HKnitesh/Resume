ededfe797603b71df5346b0d613bd2f2d1bfc772
HKnitesh <126397328+HKnitesh@users.noreply.github.com>
2023-03-30 14:57:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+// $("h2").css("color", "yellow");
+
+
+// function uppercase() {
+//     var h2 = document.querySelectorAll("h2").value;
+//     var upper = h2.toUpperCase();
+//     return upper;
+// }
+// uppercase();
+// console.log(uppercase());
+
+
